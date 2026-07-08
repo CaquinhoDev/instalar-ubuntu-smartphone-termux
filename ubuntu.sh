c3b16e0f347f659fa350deaed6c7b68fa7569dfd
Caquinho Dev <166941412+CaquinhoDev@users.noreply.github.com>
2026-07-07 22:33:07 -0300

    link fix

--- ubuntu.sh
+++ ubuntu.sh
@@ -27,7 +27,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "Arquitetura de sistema imcompativel"; exit 1 ;;
 		esac
-		wget "https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Ubuntu22/jammy-${archurl}.tar.gz" -O $tarball
+		wget "https://github.com/AndronixApp/AndronixOrigin/blob/master/Rootfs/Ubuntu22/jammy-arm64.tar.gz" -O $tarball
 
 	fi
 
